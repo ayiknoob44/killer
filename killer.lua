76d74e14f6db909c9e588197773bc6270d4e780c
ayiknoob44 <72628100+ayiknoob44@users.noreply.github.com>
2020-10-10 15:20:22 +0530

    Update killer.lua

--- killer.lua
+++ killer.lua
@@ -1,8 +1,6 @@
-function b1()
+
 d1=gg.alert("Choose One Bypass and Play Which Work For you.","okay"
 if d1 == 1 then a1()end
- KPUBGM = -1
-end
 
 fucntion a1()
 menu2 = gg.choice({
@@ -25,14 +23,4 @@ menu2 = gg.choice({
  
  function bypass3()
  end
- 
-while true do
-  if gg.isVisible(true) then
-KPUBGM = 1
-gg.setVisible(false)
-  end
-  gg.clearResults()
-  if KPUBGM == 1 then
-b1()
-  end
-end
+
