17b5f0e1366401c6a7b39d372ad0574c7d230368
ayiknoob44 <72628100+ayiknoob44@users.noreply.github.com>
2020-10-10 15:21:06 +0530

    Update killer.lua

--- killer.lua
+++ killer.lua
@@ -1,26 +1,2 @@
-
-d1=gg.alert("Choose One Bypass and Play Which Work For you.","okay"
-if d1 == 1 then a1()end
-
-fucntion a1()
-menu2 = gg.choice({
-"Bypass v1",
-"Bypass v2",
-"Bypass v3",
-"●▬▬▬●۩العوده للخلف۩▬▬▬●"},nil,"Bypass Menu - Killer PUBG")
- if menu2 == 1 then bypass1() end
- if menu2 == 2 then bypass2() end
- if menu2 == 3 then bypass3() end
- if menu2 == 4 then main() end
- KPUBGM = -1
- end
- 
- function bypass1()
- end
- 
- function bypass2()
- end
- 
- function bypass3()
- end
+gg.alert("Choose One Bypass and Play Which Work For you.","okay"
 
